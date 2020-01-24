fbf45f583463c9be184da72f5c1eb8433b11bcab
Pelin Mutanguha <pelmut2000@gmail.com>
2020-01-24 16:02:57 +0200

    create resto feature done

---
+++ OdeToFood/Services/IRestaurantData.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using System.Linq;
+using OdeToFood.Models;
+
+namespace OdeToFood.Services
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+        Restaurant Get(int id);
+
+        void Add(Restaurant newRestaurant);
+    }
+
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private readonly OdeToFoodDbContext _context;
+
+        public SqlRestaurantData(OdeToFoodDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+           return  _context.Restaurants.ToList();
+        }
+
+        public Restaurant Get(int id)
+        {
+            return _context.Restaurants.FirstOrDefault(r => r.Id == id); 
+        }
+
+        public void Add(Restaurant newRestaurant)
+        {
+            _context.Add(newRestaurant);
+            _context.SaveChanges();
+        }
+    }
+
+    class InMemoryRestaurantData : IRestaurantData
+    {
+        public InMemoryRestaurantData()
+        {
+            _restaurants = new List<Restaurant>
+            {
+                new Restaurant{Id = 1, Name = "Meze Fresh"},
+                new Restaurant{Id = 2, Name = "Camellia"},
+                new Restaurant{Id = 3, Name = "Riders Lounge"},
+                new Restaurant{Id = 4, Name = "Soleluna"},
+                new Restaurant{Id = 5, Name = "Cannaberra"}
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _restaurants;
+        }
+
+        public Restaurant Get(int id)
+        {
+            return _restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        public void Add(Restaurant newRestaurant)
+        {
+            newRestaurant.Id = _restaurants.Max(r => r.Id) + 1;
+            _restaurants.Add(newRestaurant);
+        }
+
+        static List<Restaurant> _restaurants;
+    }
+}
